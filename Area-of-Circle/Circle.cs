cfb7d032a1172f9886e5b8cc1ad62fe93db915be
lcc <sub01@iaonline.net>
2020-09-29 20:18:17 -0500

    Add project files.

---
+++ Area-of-Circle/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Area_of_Circle
+{
+    class Circle
+    {
+        public static double CalcArea(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        } // CalcArea
+
+        public static double CalcCircumference(double radius)
+        {
+            return 2 * Math.PI * radius;
+
+        } // CalcCircumference
+
+        public static double CalcDiameter(double radius)
+        {
+            return 2 * radius;
+        } // CalcDiameter
+
+    }
+}
---
+++ Area-of-Circle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Area_of_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            string userInput;
+            do
+            {
+                Console.Write("Enter a radius (in miles): ");
+                userInput = Console.ReadLine();
+                bool validFloat = double.TryParse(userInput, out radius);
+                if (!validFloat)
+                {
+                    Console.WriteLine("\nInvalid miles. Program aborting.");
+                    Environment.Exit(1);
+                }
+            } while (radius < 0);
+            double area = Circle.CalcArea(radius);
+            double circumference = Circle.CalcCircumference(radius);
+            double diameter = Circle.CalcDiameter(radius);
+            Console.WriteLine("The area of a circle of radius " + radius + " is : " + Math.Round(area, 3));
+            Console.WriteLine("The circumference of a circle of radius " + radius + " is : " + Math.Round(circumference, 3));
+            Console.WriteLine("The diameter of a circle of radius " + radius + " is : " + Math.Round(diameter, 3));
+
+            Console.Write("\nEnter miles per gallon for your car: ");
+            userInput = Console.ReadLine();
+            double mpg = double.Parse(userInput);
+            double gallons = circumference / mpg;
+            Console.WriteLine("It will take " + Math.Round(gallons, 3) + " gallons to go one time around a circle with radius of " + radius + " miles.");
+        }
+    }
+}
